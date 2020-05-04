19a515e36a058a77662dedcc07d428edb662f8fd
Stonino82 <antoninlattene@gmail.com>
2020-05-04 10:50:33 +0200

    Updates

---
+++ precache-manifest.66fd12a3270d18ad51dcec9bcaeb7cb9.js
@@ -0,0 +1,406 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccdbb3d08576e6b54d77e5bf2e6db2cb",
+    "url": "/Star-Wars-Characters/index.html"
+  },
+  {
+    "revision": "79d1f7df66792d911ced",
+    "url": "/Star-Wars-Characters/static/css/main.232c69fb.chunk.css"
+  },
+  {
+    "revision": "d38db8cd9ee454a2c8ca",
+    "url": "/Star-Wars-Characters/static/js/2.55ddc674.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Star-Wars-Characters/static/js/2.55ddc674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79d1f7df66792d911ced",
+    "url": "/Star-Wars-Characters/static/js/main.77273045.chunk.js"
+  },
+  {
+    "revision": "1c4faca46e1f530c2942",
+    "url": "/Star-Wars-Characters/static/js/runtime-main.91c8b6fd.js"
+  },
+  {
+    "revision": "053c239be325acc4e1302145e379895c",
+    "url": "/Star-Wars-Characters/static/media/1.053c239b.jpg"
+  },
+  {
+    "revision": "c5857f7d16a6c064bf27143efa18c87b",
+    "url": "/Star-Wars-Characters/static/media/1.c5857f7d.jpg"
+  },
+  {
+    "revision": "74aeb9f85778237508ee914b26018e88",
+    "url": "/Star-Wars-Characters/static/media/10.74aeb9f8.jpg"
+  },
+  {
+    "revision": "9b49cf599ab73d40545a39c30381a2ea",
+    "url": "/Star-Wars-Characters/static/media/11.9b49cf59.jpg"
+  },
+  {
+    "revision": "286110cd395ce3860aa253ffbb0031f3",
+    "url": "/Star-Wars-Characters/static/media/12.286110cd.jpg"
+  },
+  {
+    "revision": "6c2c095ecadef794f7acc26ac1649be2",
+    "url": "/Star-Wars-Characters/static/media/13.6c2c095e.jpg"
+  },
+  {
+    "revision": "edd1b2988876ce695539927a42c4b2ac",
+    "url": "/Star-Wars-Characters/static/media/14.edd1b298.jpg"
+  },
+  {
+    "revision": "17a306dd9f8c7cad41daeb07e6ff1675",
+    "url": "/Star-Wars-Characters/static/media/15.17a306dd.jpg"
+  },
+  {
+    "revision": "eff0d4282d2b146e8371fbc67977056e",
+    "url": "/Star-Wars-Characters/static/media/16.eff0d428.jpg"
+  },
+  {
+    "revision": "4ec2b99a13d62ba3ba97ad72c708237c",
+    "url": "/Star-Wars-Characters/static/media/18.4ec2b99a.jpg"
+  },
+  {
+    "revision": "33a0c6de4316bad0931baea6edfe1ec1",
+    "url": "/Star-Wars-Characters/static/media/19.33a0c6de.jpg"
+  },
+  {
+    "revision": "c3ee4e438db1be173433a814866922d5",
+    "url": "/Star-Wars-Characters/static/media/2.c3ee4e43.jpg"
+  },
+  {
+    "revision": "ce3e60472412c87c1830f521a5ea25b9",
+    "url": "/Star-Wars-Characters/static/media/2.ce3e6047.jpg"
+  },
+  {
+    "revision": "ac3f9ecd7c3b07facc44aba5c7cacb5e",
+    "url": "/Star-Wars-Characters/static/media/20.ac3f9ecd.jpg"
+  },
+  {
+    "revision": "e9768c901ee463f7a9f3c8acf9d88040",
+    "url": "/Star-Wars-Characters/static/media/21.e9768c90.jpg"
+  },
+  {
+    "revision": "bea5ebb31f70dc66b2a3e89dc9dd90b4",
+    "url": "/Star-Wars-Characters/static/media/22.bea5ebb3.jpg"
+  },
+  {
+    "revision": "93ac2bc8d76a41ebbfe4b61711695cd4",
+    "url": "/Star-Wars-Characters/static/media/23.93ac2bc8.jpg"
+  },
+  {
+    "revision": "61d16f9ecb870b3a9dd374ee56beb1d2",
+    "url": "/Star-Wars-Characters/static/media/24.61d16f9e.jpg"
+  },
+  {
+    "revision": "1a6e02aa5af7047f49d8b9909dca3ebf",
+    "url": "/Star-Wars-Characters/static/media/25.1a6e02aa.jpg"
+  },
+  {
+    "revision": "a26582f2b1e0a0fb8be24fd4ee71c916",
+    "url": "/Star-Wars-Characters/static/media/26.a26582f2.jpg"
+  },
+  {
+    "revision": "f05cd15591e9f45522cae6fa8b075b14",
+    "url": "/Star-Wars-Characters/static/media/27.f05cd155.jpg"
+  },
+  {
+    "revision": "52abcae61c45b4fc0a2ed099fde86c22",
+    "url": "/Star-Wars-Characters/static/media/28.52abcae6.jpg"
+  },
+  {
+    "revision": "e9debd5c46a63f16faf96ca8184245fa",
+    "url": "/Star-Wars-Characters/static/media/29.e9debd5c.jpg"
+  },
+  {
+    "revision": "541e2406e07f1aed0b8c933afd1a58d5",
+    "url": "/Star-Wars-Characters/static/media/3.541e2406.jpg"
+  },
+  {
+    "revision": "79c869eef60997646c59a0043bedaa22",
+    "url": "/Star-Wars-Characters/static/media/3.79c869ee.jpg"
+  },
+  {
+    "revision": "3315f4946907887381ceca72ec5121a8",
+    "url": "/Star-Wars-Characters/static/media/30.3315f494.jpg"
+  },
+  {
+    "revision": "fda97c369fbe296b044a0aee9ca6a244",
+    "url": "/Star-Wars-Characters/static/media/31.fda97c36.jpg"
+  },
+  {
+    "revision": "20c2f6606f5fcf49929903300eea7f67",
+    "url": "/Star-Wars-Characters/static/media/32.20c2f660.jpg"
+  },
+  {
+    "revision": "19a6a47d3c100aa237bfc7d4229ac8bd",
+    "url": "/Star-Wars-Characters/static/media/33.19a6a47d.jpg"
+  },
+  {
+    "revision": "5b767020433096ad95f2437053e8ccf8",
+    "url": "/Star-Wars-Characters/static/media/34.5b767020.jpg"
+  },
+  {
+    "revision": "4d91ea50b0eeb42a8a8172c7bbc160ab",
+    "url": "/Star-Wars-Characters/static/media/35.4d91ea50.jpg"
+  },
+  {
+    "revision": "912475710b186d35489e71d17db97b77",
+    "url": "/Star-Wars-Characters/static/media/36.91247571.jpg"
+  },
+  {
+    "revision": "20d4a878f2dfe33c964533d3234ee768",
+    "url": "/Star-Wars-Characters/static/media/37.20d4a878.jpg"
+  },
+  {
+    "revision": "42c4823512f639d9385e841a01c48703",
+    "url": "/Star-Wars-Characters/static/media/38.42c48235.jpg"
+  },
+  {
+    "revision": "6ec97d3bc1f38d9fd24675d764a955a1",
+    "url": "/Star-Wars-Characters/static/media/39.6ec97d3b.jpg"
+  },
+  {
+    "revision": "0f26e9152f692ca68d3559a4c8fb5407",
+    "url": "/Star-Wars-Characters/static/media/4.0f26e915.jpg"
+  },
+  {
+    "revision": "884f91924ab45cb929b4c0393a64f8f3",
+    "url": "/Star-Wars-Characters/static/media/4.884f9192.jpg"
+  },
+  {
+    "revision": "078c9e3cb1274d71e8ae2c72efaeefc8",
+    "url": "/Star-Wars-Characters/static/media/40.078c9e3c.jpg"
+  },
+  {
+    "revision": "88f3ea2d80f5f34d639a076709b1e8d4",
+    "url": "/Star-Wars-Characters/static/media/41.88f3ea2d.jpg"
+  },
+  {
+    "revision": "2f8633ba72750400a775de9249e717eb",
+    "url": "/Star-Wars-Characters/static/media/42.2f8633ba.jpg"
+  },
+  {
+    "revision": "71af17b601a48692198e671476266b6e",
+    "url": "/Star-Wars-Characters/static/media/43.71af17b6.jpg"
+  },
+  {
+    "revision": "f5f746efc6fec922f3dffc5d2836c463",
+    "url": "/Star-Wars-Characters/static/media/44.f5f746ef.jpg"
+  },
+  {
+    "revision": "116d975cd6c3ecd5c7652b974a003c05",
+    "url": "/Star-Wars-Characters/static/media/45.116d975c.jpg"
+  },
+  {
+    "revision": "56b0e2c12bf7fb6a6fdab620e7743155",
+    "url": "/Star-Wars-Characters/static/media/46.56b0e2c1.jpg"
+  },
+  {
+    "revision": "0f606ed44d666c6ea6b947287f9c2379",
+    "url": "/Star-Wars-Characters/static/media/47.0f606ed4.jpg"
+  },
+  {
+    "revision": "acde5508fac4597567757f4bfde0d45a",
+    "url": "/Star-Wars-Characters/static/media/48.acde5508.jpg"
+  },
+  {
+    "revision": "8dab78b4d0101554ab163aef2519658f",
+    "url": "/Star-Wars-Characters/static/media/49.8dab78b4.jpg"
+  },
+  {
+    "revision": "53c0b037b0f025b49c1991033a2f6ff9",
+    "url": "/Star-Wars-Characters/static/media/5.53c0b037.jpg"
+  },
+  {
+    "revision": "f95edbedd193ec9f609c234050282f90",
+    "url": "/Star-Wars-Characters/static/media/5.f95edbed.jpg"
+  },
+  {
+    "revision": "b52a712843e59f277591363ab83de5cc",
+    "url": "/Star-Wars-Characters/static/media/50.b52a7128.jpg"
+  },
+  {
+    "revision": "304cf85a8711267467103b2145bf90f6",
+    "url": "/Star-Wars-Characters/static/media/51.304cf85a.jpg"
+  },
+  {
+    "revision": "1200ec7c1f5dc24d6b7e8f8da979d3ac",
+    "url": "/Star-Wars-Characters/static/media/52.1200ec7c.jpg"
+  },
+  {
+    "revision": "8c478726f580f680ab75f9809ece1ce4",
+    "url": "/Star-Wars-Characters/static/media/53.8c478726.jpg"
+  },
+  {
+    "revision": "ed4e6f762b9e343c25388f8e09c74a6b",
+    "url": "/Star-Wars-Characters/static/media/54.ed4e6f76.jpg"
+  },
+  {
+    "revision": "2f8fc579eb2dc5811c896c890175f3c4",
+    "url": "/Star-Wars-Characters/static/media/55.2f8fc579.jpg"
+  },
+  {
+    "revision": "d28209d498be528351ddcb7c2817499c",
+    "url": "/Star-Wars-Characters/static/media/56.d28209d4.jpg"
+  },
+  {
+    "revision": "8f19f58eb87b64fcbe92c27676f437db",
+    "url": "/Star-Wars-Characters/static/media/57.8f19f58e.jpg"
+  },
+  {
+    "revision": "41a38b5ad304341607439f55f61e84c0",
+    "url": "/Star-Wars-Characters/static/media/58.41a38b5a.jpg"
+  },
+  {
+    "revision": "d2e213f0e1fbe329dc4f293993e36142",
+    "url": "/Star-Wars-Characters/static/media/59.d2e213f0.jpg"
+  },
+  {
+    "revision": "638a556b4576fe32aac0abb0ce327856",
+    "url": "/Star-Wars-Characters/static/media/6.638a556b.jpg"
+  },
+  {
+    "revision": "dac799747cc03339b1a85b0887b7d05c",
+    "url": "/Star-Wars-Characters/static/media/6.dac79974.jpg"
+  },
+  {
+    "revision": "57b527fd70eaef2ea7c5814b0d4a5390",
+    "url": "/Star-Wars-Characters/static/media/60.57b527fd.jpg"
+  },
+  {
+    "revision": "cc700283f5e3b31d4d7ac79aa07ce9aa",
+    "url": "/Star-Wars-Characters/static/media/61.cc700283.jpg"
+  },
+  {
+    "revision": "141b76bef7fb8c3c06d96e73a506c0fe",
+    "url": "/Star-Wars-Characters/static/media/62.141b76be.jpg"
+  },
+  {
+    "revision": "df6917cc09c7e3f2723f54c3bedb5da4",
+    "url": "/Star-Wars-Characters/static/media/63.df6917cc.jpg"
+  },
+  {
+    "revision": "3b9ecf63d2b87babc6e13c0a36c6f468",
+    "url": "/Star-Wars-Characters/static/media/64.3b9ecf63.jpg"
+  },
+  {
+    "revision": "134b976fab5bebf0d7094e6cf985a07e",
+    "url": "/Star-Wars-Characters/static/media/65.134b976f.jpg"
+  },
+  {
+    "revision": "f032dbf2de1c9961efd1189c80d7d3ef",
+    "url": "/Star-Wars-Characters/static/media/66.f032dbf2.jpg"
+  },
+  {
+    "revision": "3305c13a6c3fe00aa91604414d86e09d",
+    "url": "/Star-Wars-Characters/static/media/67.3305c13a.jpg"
+  },
+  {
+    "revision": "d5076cc581a1d55f123a091b42ce23bb",
+    "url": "/Star-Wars-Characters/static/media/68.d5076cc5.jpg"
+  },
+  {
+    "revision": "7b7bf1fa443cce98e86a73295747fd78",
+    "url": "/Star-Wars-Characters/static/media/69.7b7bf1fa.jpg"
+  },
+  {
+    "revision": "8f4b4f89f7b9389906039ca8f0752854",
+    "url": "/Star-Wars-Characters/static/media/7.8f4b4f89.jpg"
+  },
+  {
+    "revision": "9b3e5ff459bdde43e6a806ed3426b7da",
+    "url": "/Star-Wars-Characters/static/media/7.9b3e5ff4.jpg"
+  },
+  {
+    "revision": "a629f191f6d05f3edbc20417a82e3200",
+    "url": "/Star-Wars-Characters/static/media/70.a629f191.jpg"
+  },
+  {
+    "revision": "3d354b95dc9c6da2664a7838e3b1e48a",
+    "url": "/Star-Wars-Characters/static/media/71.3d354b95.jpg"
+  },
+  {
+    "revision": "90166dc1b3d54e67b436724791a0b4d5",
+    "url": "/Star-Wars-Characters/static/media/72.90166dc1.jpg"
+  },
+  {
+    "revision": "1c87faa0f4ef92fceb9d618fd8474b6c",
+    "url": "/Star-Wars-Characters/static/media/73.1c87faa0.jpg"
+  },
+  {
+    "revision": "a34f7ccbc76e3b445b0ea050e6d182cc",
+    "url": "/Star-Wars-Characters/static/media/74.a34f7ccb.jpg"
+  },
+  {
+    "revision": "c612d4bc38d037f2b59641cda30744a5",
+    "url": "/Star-Wars-Characters/static/media/75.c612d4bc.jpg"
+  },
+  {
+    "revision": "e0cd42d91cddfae12f072726b301b1df",
+    "url": "/Star-Wars-Characters/static/media/76.e0cd42d9.jpg"
+  },
+  {
+    "revision": "b63e685db5497bd6e46b80c2992eba69",
+    "url": "/Star-Wars-Characters/static/media/77.b63e685d.jpg"
+  },
+  {
+    "revision": "b7f7006cf0a8a3dd19f0701271ac85f4",
+    "url": "/Star-Wars-Characters/static/media/78.b7f7006c.jpg"
+  },
+  {
+    "revision": "60f9ab69934df86c295c52453f1d4eba",
+    "url": "/Star-Wars-Characters/static/media/79.60f9ab69.jpg"
+  },
+  {
+    "revision": "210c16d20a0f65f2050093c10f209b7e",
+    "url": "/Star-Wars-Characters/static/media/8.210c16d2.jpg"
+  },
+  {
+    "revision": "ccf3d7cdf4cb03243d2d3ddabf936d6f",
+    "url": "/Star-Wars-Characters/static/media/80.ccf3d7cd.jpg"
+  },
+  {
+    "revision": "7659eb5c0f4c4bb67add2df3db0ce7cb",
+    "url": "/Star-Wars-Characters/static/media/81.7659eb5c.jpg"
+  },
+  {
+    "revision": "3e27dcbde402bc4e272db14cdf322c4a",
+    "url": "/Star-Wars-Characters/static/media/82.3e27dcbd.jpg"
+  },
+  {
+    "revision": "8dc8131e07b551f6f1ca069d4e0e89b0",
+    "url": "/Star-Wars-Characters/static/media/83.8dc8131e.jpg"
+  },
+  {
+    "revision": "342fc9640d37cc456fd4c1bb406869f2",
+    "url": "/Star-Wars-Characters/static/media/84.342fc964.jpg"
+  },
+  {
+    "revision": "71bcc52f9f5349f130d7b24029d8cdf7",
+    "url": "/Star-Wars-Characters/static/media/85.71bcc52f.jpg"
+  },
+  {
+    "revision": "92350baec0c45dc1319517c29a84ab44",
+    "url": "/Star-Wars-Characters/static/media/86.92350bae.jpg"
+  },
+  {
+    "revision": "86b756d75b6ca664faaf11b6cb399259",
+    "url": "/Star-Wars-Characters/static/media/87.86b756d7.jpg"
+  },
+  {
+    "revision": "47cbba0b747cab53682cc59b3335ce4f",
+    "url": "/Star-Wars-Characters/static/media/88.47cbba0b.jpg"
+  },
+  {
+    "revision": "daac3de8db19ac57fada2bcd31f13903",
+    "url": "/Star-Wars-Characters/static/media/9.daac3de8.jpg"
+  },
+  {
+    "revision": "6494ab5b3760b5e2cc46f5566a245371",
+    "url": "/Star-Wars-Characters/static/media/stars.6494ab5b.png"
+  }
+]);
