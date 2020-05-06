a6dc1fa2bcb4a3412421faf48d6aa0a662f17bfc
Stonino82 <antoninlattene@gmail.com>
2020-05-06 16:17:11 +0200

    links update

--- src/App.js
+++ src/App.js
@@ -105,8 +105,8 @@ class App extends Component {
         <div className="footer">
           <div>
             <p>Designed and developed by <a href="http://www.antoninolattene.com">Antonino Lattene</a> @2020</p>
-            <a href=""><i className="fab fa-github"></i></a>
-            <a href=""><i className="fab fa-linkedin-in"></i></a>
+            <a href="https://github.com/Stonino82"><i className="fab fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/antoninolattene/"><i className="fab fa-linkedin-in"></i></a>
           </div>
           <p>Star Wars and all associated names and/or images are copyright Lucasfilm Ltd.</p>
         </div>
